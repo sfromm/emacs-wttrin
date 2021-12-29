fd86ff800a900b1ba9ff10d4f0d471d9fa9ed3c0
Stephen Fromm <stephenf@nero.net>
2021-12-29 09:23:34 -0800

    apply fix to not display raw html
    fix from https://github.com/bcbcarl/emacs-wttrin/pull/20

--- wttrin.el
+++ wttrin.el
@@ -35,11 +35,11 @@
 
 (defun wttrin-fetch-raw-string (query)
   "Get the weather information based on your QUERY."
-  (let ((url-request-extra-headers '(("User-Agent" . "curl"))))
+  (let ((url-user-agent "curl"))
     (add-to-list 'url-request-extra-headers wttrin-default-accept-language)
     (with-current-buffer
         (url-retrieve-synchronously
-         (concat "http://wttr.in/" query)
+         (concat "http://wttr.in/" query "?A")
          (lambda (status) (switch-to-buffer (current-buffer))))
       (decode-coding-string (buffer-string) 'utf-8))))
 
